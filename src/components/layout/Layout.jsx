0cf6b1cddb4469c6c1ef056f56f94f87c6091883
Kenneth J. Eilor <kennetheilor@gmail.com>
2025-11-02 12:43:22 +0300

    11/02/2025 We build to finish.

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,41 +2,45 @@ import React, { useState, useEffect } from "react";
 import Sidebar from "./sidebar/Sidebar";
 import Main from "./main/Main";
 import Splash from "../splash/Splash.jsx";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 import "./styles/Layout.scss";
 
 function Layout() {
   const [showSplash, setShowSplash] = useState(true);
-  const [isSidebarVisible, setSidebarVisible] = useState(true); // desktop sidebar
-  const [isMobileSidebarOpen, setMobileSidebarOpen] = useState(false); // mobile sidebar
+  const [isSidebarVisible, setSidebarVisible] = useState(true);
+  const [isMobileSidebarOpen, setMobileSidebarOpen] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => setShowSplash(false), 1500);
     return () => clearTimeout(timer);
   }, []);
 
-  const toggleSidebarDesktop = () => {
-    setSidebarVisible(prev => !prev);
-  };
+  const toggleSidebarDesktop = () => setSidebarVisible(p => !p);
+  const toggleSidebarMobile = () => setMobileSidebarOpen(p => !p);
 
-  const toggleSidebarMobile = () => {
-    setMobileSidebarOpen(prev => !prev);
-  };
+  const isOpen = window.innerWidth <= 768 ? isMobileSidebarOpen : isSidebarVisible;
 
   if (showSplash) return <Splash />;
 
   return (
-    <div
-      className={`layout
-        ${!isSidebarVisible ? "sidebar-hidden" : ""}
-        ${isMobileSidebarOpen ? "sidebar-open-mobile" : ""}
-      `}
-    >
+    <div className="layout">
+      {/* Pass the correct open/close flag down */}
       <Sidebar
+        isSidebarVisible={window.innerWidth <= 768 ? isMobileSidebarOpen : isSidebarVisible}
         toggleSidebar={toggleSidebarDesktop}
-        isSidebarVisible={isSidebarVisible}
         sidebarOpen={isMobileSidebarOpen}
         toggleSidebarMobile={toggleSidebarMobile}
       />
+
+      {/* Single floating toggle */}
+      <button
+        className="layout__sidebar-toggle"
+        onClick={window.innerWidth <= 768 ? toggleSidebarMobile : toggleSidebarDesktop}
+      >
+        {isOpen ? <CloseOutlinedIcon fontSize="small" /> : <MenuOutlinedIcon fontSize="small" />}
+      </button>
+
       <div className="layout__content">
         <Main />
       </div>
--- src/components/layout/sidebar/Sidebar.jsx
+++ src/components/layout/sidebar/Sidebar.jsx
@@ -11,118 +11,75 @@ import ViewInArOutlinedIcon from "@mui/icons-material/ViewInArOutlined";
 import CampaignOutlinedIcon from "@mui/icons-material/CampaignOutlined";
 import DirectionsBoatOutlinedIcon from "@mui/icons-material/DirectionsBoatOutlined";
 import InsertDriveFileOutlinedIcon from "@mui/icons-material/InsertDriveFileOutlined";
-import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import "../styles/Sidebar.scss";
 
-function Sidebar({
-  isSidebarVisible,
-  toggleSidebar,
-  sidebarOpen,
-  toggleSidebarMobile,
-}) {
+function Sidebar({ isSidebarVisible }) {
   return (
-    <>
-      {/* Toggle Button */}
-      <button className="sidebar-toggle" onClick={toggleSidebarMobile}>
-        <MenuOutlinedIcon />
-      </button>
+    <div className={`sidebar ${!isSidebarVisible ? "hidden" : ""}`}>
+      <div className="sidebar-content">
+        <div className="biz-prof"><BizProfile /></div>
+        <div className="menu-bar">
+          <div className="menu-wrp">
+            <NavLink id="menu-itm" to="/Updates">
+              <NotificationsOutlinedIcon />
+              <span className="title m-name">Updates</span>
+              <div className="badge upt-bdg"></div>
+            </NavLink>
 
-      <div
-        className={`sidebar ${isSidebarVisible ? "" : "hidden"} ${
-          sidebarOpen ? "open" : ""
-        }`}
-      >
-        <div className="sidebar-content">
-          <div className="biz-prof">
-            <BizProfile />
-          </div>
-          <div className="menu-bar">
-            <div className="menu-wrp">
-              <div>
-                <NavLink id="menu-itm" to="/Updates">
-                  <div className="m-icn">
-                    <NotificationsOutlinedIcon />
-                  </div>
-                  <span className="title m-name">Updates</span>
-                  <div className="badge upt-bdg"></div>
-                </NavLink>
-              </div>
+            <NavLink id="menu-itm" to="/general">
+              <SettingsOutlinedIcon />
+              <span className="title m-name">General</span>
+              <div className="badge upt-bdg"></div>
+            </NavLink>
 
-              <div>
-                <NavLink id="menu-itm" to="/general">
-                  <div className="m-icn">
-                    <SettingsOutlinedIcon />
-                  </div>
-                  <span className="title m-name">General</span>
-                  <div className="badge upt-bdg"></div>
-                </NavLink>
-              </div>
+            <NavLink id="menu-itm" to="/explore">
+              <ExploreOutlinedIcon />
+              <span className="title m-name">Explore</span>
+              <div className="badge upt-bdg"></div>
+            </NavLink>
 
-              <div>
-                <NavLink id="menu-itm" to="/explore">
-                  <div className="m-icn">
-                    <ExploreOutlinedIcon />
-                  </div>
-                  <span className="title m-name">Explore</span>
-                  <div className="badge upt-bdg"></div>
-                </NavLink>
-              </div>
-
-              <div className="divider">
-                <h4 className="title m-title">Workstation</h4>
-              </div>
-
-              <NavLink id="menu-itm" to="/storebook">
-                <div className="m-icn">
-                  <StorefrontOutlinedIcon />
-                </div>
-                <span className="title m-name">Storebook</span>
-              </NavLink>
-
-              <NavLink id="menu-itm" to="/teams">
-                <div className="m-icn">
-                  <PeopleOutlineOutlinedIcon />
-                </div>
-                <span className="title m-name">Teams</span>
-              </NavLink>
-
-              <NavLink id="menu-itm" to="/catalogue">
-                <div className="m-icn">
-                  <ViewInArOutlinedIcon />
-                </div>
-                <span className="title m-name">Catalogue</span>
-              </NavLink>
-
-              <NavLink id="menu-itm" to="/marketing">
-                <div className="m-icn">
-                  <CampaignOutlinedIcon />
-                </div>
-                <span className="title m-name">Marketing</span>
-              </NavLink>
-
-              <NavLink id="menu-itm" to="/supplydock">
-                <div className="m-icn">
-                  <DirectionsBoatOutlinedIcon />
-                </div>
-                <span className="title m-name">Supply Dock</span>
-              </NavLink>
-
-              <NavLink id="menu-itm" to="/documents">
-                <div className="m-icn">
-                  <InsertDriveFileOutlinedIcon />
-                </div>
-                <span className="title m-name">Documents</span>
-              </NavLink>
+            <div className="divider">
+              <h4 className="title m-title">Workstation</h4>
             </div>
-          </div>
-          <div className="usr-prof">
-            <NavLink to="/profile/user" className="user-link">
-              <UserProfile />
+
+            <NavLink id="menu-itm" to="/storebook">
+              <StorefrontOutlinedIcon />
+              <span className="title m-name">Storebook</span>
+            </NavLink>
+
+            <NavLink id="menu-itm" to="/teams">
+              <PeopleOutlineOutlinedIcon />
+              <span className="title m-name">Teams</span>
+            </NavLink>
+
+            <NavLink id="menu-itm" to="/catalogue">
+              <ViewInArOutlinedIcon />
+              <span className="title m-name">Catalogue</span>
+            </NavLink>
+
+            <NavLink id="menu-itm" to="/marketing">
+              <CampaignOutlinedIcon />
+              <span className="title m-name">Marketing</span>
+            </NavLink>
+
+            <NavLink id="menu-itm" to="/supplydock">
+              <DirectionsBoatOutlinedIcon />
+              <span className="title m-name">Supply Dock</span>
+            </NavLink>
+
+            <NavLink id="menu-itm" to="/documents">
+              <InsertDriveFileOutlinedIcon />
+              <span className="title m-name">Documents</span>
             </NavLink>
           </div>
         </div>
+        <div className="usr-prof">
+          <NavLink to="/profile/user" className="user-link">
+            <UserProfile />
+          </NavLink>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
